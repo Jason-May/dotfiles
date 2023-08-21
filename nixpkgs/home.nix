8c7633ab88c7cad69c5521b9efdd9f9ea5bfd77c
Jason <jwmay@ucsc.edu>
2023-08-20 22:31:30 -0700

    moved kitty into home manager

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,14 +1,44 @@
-{ pkgs, ...}: {
+{ pkgs, ...}: 
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz";
+in
+{
   home = {
     username = "jason";
     homeDirectory = "/home/jason";
   };
 
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    firefox
+    discord
 
+    screen
+    gtkterm
+
+    python37
+    haskell.compiler.ghc902
+    cabal-install
+
+    qbittorrent
+    lutris
+  ];
+
+  programs.kitty = {
+    enable = true;
+    extraConfig = "
+      font_size 16
+      window_padding_width 8
+      hide_window_decorations yes
+
+      remember_window_size no
+      initial_window_width 800
+      initial_window_height 460
+    ";    
+  };
 
   # let home manager manage fish
   # for env variables to work
   programs.fish.enable = true;
-  programs.home-manager.enable = true;
   home.stateVersion = "22.11";
 }
