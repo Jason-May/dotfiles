a1469bfb43d818e6b3b8e2ed84e8d74c9cad38c4
Jason <jwmay@ucsc.edu>
2024-04-22 22:59:07 -0700

    expanded home manager config to multiple files

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,40 +7,9 @@ in
     username = "jason";
     homeDirectory = "/home/jason";
   };
-
-  nixpkgs.config.allowUnfree = true;
-  home.packages = with pkgs; [
-    firefox
-    discord
-
-    screen
-    gtkterm
-
-    haskell.compiler.ghc902
-    cabal-install
-    cargo
-    rustc
-    gcc
-
-    vlc
-    qbittorrent
-    lutris
-    blender
-    prusa-slicer
+  imports = [
+    ./packages/packages.nix
   ];
-
-  programs.kitty = {
-    enable = true;
-    extraConfig = "
-      font_size 16
-      window_padding_width 8
-      hide_window_decorations yes
-
-      remember_window_size no
-      initial_window_width 800
-      initial_window_height 460
-    ";    
-  };
 
   # let home manager manage fish
   # for env variables to work
---
+++ home-manager/packages/packages.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ...}: 
+{
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    firefox
+    discord
+    screen
+    gtkterm
+    haskell.compiler.ghc902
+    cabal-install
+    cargo
+    rustc
+    gcc
+    vlc
+    qbittorrent
+    lutris
+    blender
+    prusa-slicer  
+    ssh-add
+  ];
+  imports = [
+    ./kitty.nix
+  ];
+}
